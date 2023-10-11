a92789c014d8c7127561dc32d9020268efbd40b8
k <keeroo1337@googlemail.com>
2023-10-11 16:50:34 +0200

    trades, volume, width, and index

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from datetime import time
-import datetime
-from fastapi import FastAPI
+from datetime import datetime, timedelta
 from time import sleep
 import requests
 import json
@@ -9,30 +7,72 @@ import json
 # width>1.5 und volume >120 und trades >200 in den letzten 15 minuten
 
 
-app = FastAPI()
-url = "https://api.hitbtc.com/api/3/public/trades"
-r = requests.get(url).content
-j = json.loads(r)
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
+    fmat = '%Y-%m-%dT%H:%M:%S.%fZ'
     while True:
+        print('start')
+        url = "https://api.hitbtc.com/api/3/public/trades?limit=1000&by=timestamp&sort=DESC"
+        end = (datetime.now() - timedelta(hours=2)).strftime(fmat)
+        start = (datetime.now() - timedelta(hours=2, minutes=15)).strftime(fmat)
+        url = url + '&from=' + start + '&till=' + end
+        r = requests.get(url).content
+        print(url)
+        # print(r)
+        j = json.loads(r)
         for i in j:
             a = j[i]
-            tradecounter = 0
+            tradecounter = float(0)
+            counter = 0
+            volume = 0
+            buy = 0
+            sell = 0
+            buycounter = 0
+            sellcounter = 0
+            sellwidth = 0
+            buywidth = 0
             for m in a:
+                counter = counter + 1
+                # process timestamp
                 t = m['timestamp']
-                time.strptime(t)
-                print(t)
-                tradecounter = tradecounter + 1
-            print(i + "\ttrades:" + str(tradecounter))
+                d = datetime.strptime(t, '%Y-%m-%dT%H:%M:%S.%fZ')
+                now = datetime.today()
+                diff = now - d
+                # difference to be in the last 15 minutes
+                #print (str(diff - timedelta(hours=2)))
+                if diff - timedelta(hours=2, minutes=15) < timedelta(minutes=15):
+                    tradecounter = tradecounter + 1
+
+                # process volume
+                price = m['price']
+                #print(price)
+                qty = m['qty']
+                #print(qty)
+                volume += float(price)*float(qty)
+                #print(volume)
+
+                side = m['side']
+                if side == 'buy':
+                    buy += float(price)
+                    buycounter += 1
+                elif side == 'sell':
+                    sell += float(price)
+                    sellcounter += 1
+
+            if buycounter != 0:
+                buywidth = float(buy / buycounter)
+
+            if sellcounter != 0:
+                sellwidth = float(sell / sellcounter)
+
+            if tradecounter > 100 and ((sellwidth - buywidth) * volume) > 0:
+                print(i
+                      + '\tTrades: ' + str(tradecounter)
+                      + '\tVolume: ' + str(volume)
+                      + '\tBuy: ' + str(buywidth)
+                      + '\tSell: ' + str(sellwidth)
+                      + '\tWidth: ' + str(sellwidth - buywidth)
+                      + '\tIndex: ' + str((sellwidth - buywidth) * volume)
+                      )
+        print('end')
         sleep(5)
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
