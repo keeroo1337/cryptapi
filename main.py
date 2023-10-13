28b1fb7ab75e0f7a913b6169ac3f8b93704a448c
k <keeroo1337@googlemail.com>
2023-10-13 12:56:44 +0200

    changed to cgi app

--- main.py
+++ main.py
@@ -1,83 +1,84 @@
-from datetime import datetime, timedelta
-from time import sleep
-import requests
+#!/usr/bin/python3
+
 import json
-from flask import Flask
+from datetime import datetime, timedelta
+import urllib.request
 
-app = Flask(__name__)
+print("Content-type: text/html")
+print("")
+print("<html><head><title>CGI</title></head>")
+print("<body>")
+
 fmat = '%Y-%m-%dT%H:%M:%S.%fZ'
+url = "https://api.hitbtc.com/api/3/public/trades?limit=1000&by=timestamp&sort=DESC"
 
-@app.route("/")
-def index():
-    url = "https://api.hitbtc.com/api/3/public/trades?limit=1000&by=timestamp&sort=DESC"
-    end = (datetime.now() - timedelta(hours=2)).strftime(fmat)
-    start = (datetime.now() - timedelta(hours=2, minutes=15)).strftime(fmat)
-    url = url + '&from=' + start + '&till=' + end
-    print(url)
-    s = ''
-    r = requests.get(url).content
-    s += url
-    tradenum = 100
-    threshold = 0
-    timeframe = 15
-    printer = ('Parameters:<br>'
-               + 'Trades: ' + str(tradenum) + ' Index Threshold > ' + str(threshold))
-    print(printer)
-    s += '<br>' + printer
-    j = json.loads(r)
-    counter = 0
-    for i in j:
-        a = j[i]
-        tradecounter = float(0)
-        volume = 0
-        buy = 0
-        sell = 0
-        buycounter = 0
-        sellcounter = 0
-        sellwidth = 0
-        buywidth = 0
-        for m in a:
-            # process timestamp
-            t = m['timestamp']
-            d = datetime.strptime(t, '%Y-%m-%dT%H:%M:%S.%fZ')
-            now = datetime.today()
-            diff = now - d
-            # difference to be in the last 15 minutes
-            # print('Diff is ' + str(diff - timedelta(hours=2)))
-            if diff - timedelta(hours=2, minutes=15) < timedelta(minutes=15):
-                tradecounter = tradecounter + 1
-                # process volume
-                price = m['price']
-                # print(price)
-                qty = m['qty']
-                # print(qty)
-                volume += float(price)*float(qty)
-                # print(volume)
-                side = m['side']
-                if side == 'buy':
-                    buy += float(price)
-                    buycounter += 1
-                elif side == 'sell':
-                    sell += float(price)
-                    sellcounter += 1
-                if buycounter != 0:
-                    buywidth = float(buy / buycounter)
-                if sellcounter != 0:
-                    sellwidth = float(sell / sellcounter)
-        if tradecounter > tradenum and ((sellwidth - buywidth) * volume) > threshold:
-            s += ('<br><br>'
-                  + '<b>' + i + '</b>'
-                  + '<br>&nbsp;&nbsp;&nbsp;&nbsp;<i>Trades:</i>' + str(tradecounter)
-                  + '<br>&nbsp;&nbsp;&nbsp;&nbsp;<i>Volume</i>: ' + str(volume)
-                  + '<br>&nbsp;&nbsp;&nbsp;&nbsp;<i>Buy</i>: ' + str(buywidth)
-                  + '<br>&nbsp;&nbsp;&nbsp;&nbsp;<i>Sell</i>: ' + str(sellwidth)
-                  + '<br>&nbsp;&nbsp;&nbsp;&nbsp;<i>Width</i>: ' + str(sellwidth - buywidth)
-                  + '<br>&nbsp;&nbsp;&nbsp;&nbsp;<i>Index</i>: ' + str((sellwidth - buywidth) * volume)
-                  )
-    return s
+end = (datetime.now() - timedelta(hours=2)).strftime(fmat)
+start = (datetime.now() - timedelta(hours=2, minutes=15)).strftime(fmat)
+url = url + '&from=' + start + '&till=' + end
+print("<a target=_blank href='" + url + "'>" + url + "</a><br>")
+res = urllib.request.urlopen(url)
+res_body = res.read()
 
-# checke jede 5 mins https://api.hitbtc.com/#trades auf
-# width>1.5 und volume >120 und trades >200 in den letzten 15 minuten
-if __name__ == '__main__':
-    app.run()
+tradenum = 100
+threshold = 0.1
 
+j = json.loads(res_body)
+
+for i in j:
+	a = j[i]
+	tradecounter = 0
+	volume = 0
+	buy = 0
+	sell = 0
+	buycounter = 0
+	sellcounter = 0
+	sellwidth = 0
+	buywidth = 0
+	for m in a:
+		# process timestamp
+		t = m['timestamp']
+		d = datetime.strptime(t, '%Y-%m-%dT%H:%M:%S.%fZ')
+		now = datetime.today()
+		diff = now - d
+		# difference to be in the last 15 minutes
+		# print('Diff is ' + str(diff - timedelta(hours=2)))
+		if diff - timedelta(hours=2, minutes=15) < timedelta(minutes=15):
+			tradecounter = tradecounter + 1
+		# process volume
+		price = m['price']
+		# print(price)
+		qty = m['qty']
+		# print(qty)
+		volume += float(price) * float(qty)
+		# print(volume)
+		side = m['side']
+		if side == 'buy':
+			buy += float(price)
+			buycounter += 1
+		elif side == 'sell':
+			sell += float(price)
+			sellcounter += 1
+		if buycounter != 0:
+			buywidth = float(buy / buycounter)
+		if sellcounter != 0:
+			sellwidth = float(sell / sellcounter)
+	index = abs((sellwidth - buywidth) * volume)
+	res = ('<br><br>'
+		+ '<b>' + i + '</b>'
+		+ '<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>Trades</i>: ' + str(tradecounter)
+		+ '<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>Buys</i>: ' + str(buycounter)
+		+ '<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>Sells</i>: ' + str(sellcounter)
+		+ '<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>Volume</i>: ' + str(volume)
+		+ '<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>Buy Width</i>: ' + str(buywidth)
+		+ '<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>Sell Width</i>: ' + str(sellwidth)
+		+ '<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>Width Average</i>: ' + str(abs(sellwidth - buywidth))
+		+ '<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>Index</i>: ' + str(index))
+	if tradecounter > tradenum and index > threshold:
+		if not i.endswith('PERP'):
+			print(res)
+	elif i.endswith('TRXUSDT') or i.endswith('REEFUSDT') or i.endswith('CHSBBTC') or i.endswith('AVAXUSDT'):
+		print(res)
+
+
+print("</body>")
+print("</html>")
